6ace83c1a306083c999e64611955bb736dbc4501
Douglas Lovera <doulovera@gmail.com>
2024-10-06 20:24:54 -0500

    temporary fix qr-list components

--- src/components/qr-list/list.tsx
+++ src/components/qr-list/list.tsx
@@ -1,24 +1,6 @@
-'use client'
-
-import { Suspense, lazy } from "react";
-import { useSearchParams } from "next/navigation";
-import useAuth from "@/hooks/useAuth";
-
-/// @ts-ignore
-const QrItem = lazy(() => import('@/components/qr-list/qr-item'))
-
 
 export const List = ({ id }: { id: string }) => {
-  const user = useAuth();
+  const user = false;
   
-  if (!user || !user.accessToken) return null
-
-  return (
-    <Suspense fallback={<p>Loading in the Suspense...</p>}>
-      <QrItem
-        user={user}
-        id={id}
-      />
-    </Suspense>
-  )
+  if (!user) return <p>not logged in</p> // TODO
 }
--- src/components/qr-list/qr-item.tsx
+++ src/components/qr-list/qr-item.tsx
@@ -1,65 +1,8 @@
 'use server'
 
-import { QrItemContent } from "./qr-item-content";
-
 export default async function QrItem (
   { user, id }:
   { user: { accessToken: string }, id: string }
 ) {
-  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || ""
-
-  const res = await fetch(
-    `${API_BASE_URL}/gen/info/${id}?img=true`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${user.accessToken}`
-      },
-      next: { revalidate: 0 },
-      cache: 'no-store',
-    }
-  )
-
-  if (!res.ok) {
-    console.error('Error fetching QR code')
-    return null
-  }
-
-  const data: Response = await res.json()
-
-  if (!data.success) {
-    console.error('Error fetching QR code')
-    return null
-  }
-
-  const response = data.response
-
-  const visitorsCount = 0
-
-  return (
-    <>
-      <QrItemContent
-        visitorsCount={0}
-        {...response}
-      />
-    </>
-  )
-}
-
-/// TODO: put in correct file
-export type QrInfoResponse = {
-  id: string;
-  created: string | undefined;
-  disabled: boolean;
-  destinationUrl: string;
-  length: number
-  page: number
-  image: string
-  next: string | null
-  previous: string | null
-}
-type Response = {
-  success: boolean
-  response: QrInfoResponse
+  return <p>test</p>
 }
