433bb9d7cb9cd9264fa008c75e922324d5a2384a
Douglas Lovera <doulovera@gmail.com>
2024-11-10 02:36:51 -0500

    refactor QR generation form to simplify URL handling and improve button styling

--- src/components/landing/hero/qr-form.tsx
+++ src/components/landing/hero/qr-form.tsx
@@ -4,8 +4,6 @@ import { API_BASE_URL } from "@/constants/app";
 import { createQR } from "@/data/actions/create-qr-actions";
 import { createPermaQR } from "@/data/actions/perma-code-actions";
 /* components */
-import { LightningBolt } from "@/components/icons/lightning-bolt";
-import { Qr } from "@/components/icons/qr";
 import { Button } from "@/components/shared/button";
 import { Input } from "@/components/shared/input";
 
@@ -17,9 +15,9 @@ export function QrGenerationForm({ setSvg }: { setSvg: (svg: string | null) => v
   const handleSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault()
     const form = evt.currentTarget
-    const url = form.url.value
+    const { value } = form.url
 
-    const data = await createQR({ url })
+    const data = await createQR({ url: value })
     if (!data.success) return
 
     setSvg(data.svg)
@@ -34,15 +32,17 @@ export function QrGenerationForm({ setSvg }: { setSvg: (svg: string | null) => v
   return (
     <form className="flex-1 flex flex-col gap-2" onSubmit={handleSubmit}>
       <h1 className="text-3xl font-bold">Generate a QR</h1>
+
       <Input
         name="url"
         label="Destination URL"
         onChange={(evt) => setDestinationUrl(evt.target.value)}
       />
+
       <div className="flex gap-2">
         <Button type="submit" disabled={!API_BASE_URL}>
-          <span className="flex gap-2">
-            Generate <Qr width={20} color="#fff" />
+          <span className="flex gap-2 px-5">
+            Generate
           </span>
         </Button>
 
@@ -52,11 +52,12 @@ export function QrGenerationForm({ setSvg }: { setSvg: (svg: string | null) => v
           disabled={permaQrDisabled}
           onClick={handlePermaQr}
         >
-          <span className="flex gap-2">
-            PermaQR <LightningBolt width={20} color="#fff" />
+          <span className="flex gap-2 px-5">
+            PermaQR
           </span>
         </Button>
       </div>
+
     </form>
   )
 }
