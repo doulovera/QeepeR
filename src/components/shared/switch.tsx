e563f968dd622cab28c973dbf8681d404cc63fba
doulovera <doulovera@gmail.com>
2025-02-08 14:20:40 -0500

    fix: switch thumb when description is passed

--- src/components/shared/switch.tsx
+++ src/components/shared/switch.tsx
@@ -29,17 +29,19 @@ export const Switch = ({
     <label
       className={`relative inline-flex items-center w-full max-w-full ${disabled ? 'opacity-80 cursor-not-allowed' : 'cursor-pointer'}`}
     >
-      <input
-        name={name}
-        type="checkbox"
-        checked={isChecked}
-        className="sr-only peer"
-        disabled={disabled}
-        onChange={handleChange}
-      />
-      <div
-        className="peer inline-flex items-center shrink-0 h-6 w-12 rounded-full border-2 border-base-400 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 peer-checked:bg-primary-400 bg-primary-100 cursor-pointer peer-checked:after:translate-x-[130%] rtl:peer-checked:after:-translate-x-full after:content-[''] after:absolute after:top-[4px] after:start-[6px] after:bg-white after:border-black after:border-2 after:rounded-lg after:h-4 after:w-4 after:transition-all"
-      />
+      <div className="relative">
+        <input
+          name={name}
+          type="checkbox"
+          checked={isChecked}
+          className="sr-only peer disabled:cursor-not-allowed disabled:opacity-50"
+          disabled={disabled}
+          onChange={handleChange}
+        />
+        <div
+          className="peer inline-flex items-center shrink-0 h-6 w-12 rounded-full border-2 border-base-400 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 peer-checked:bg-primary-400 bg-primary-100 cursor-pointer peer-checked:after:translate-x-[130%] rtl:peer-checked:after:-translate-x-full after:content-[''] after:absolute after:top-[4px] after:start-[6px] after:bg-white after:border-black after:border-2 after:rounded-lg after:h-4 after:w-4 after:transition-all"
+        />
+      </div>
       <div className="ms-3">
         <span className="text-sm font-bold">{label}</span>
         {description ? <p className="text-xs">{description}</p> : null}
