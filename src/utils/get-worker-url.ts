1ff499ccbe2b18873e8394d7041e6c42bd9a468e
doulovera <doulovera@gmail.com>
2025-02-22 20:49:50 -0500

    fix: correct worker URL construction by adding a leading slash

--- src/utils/get-worker-url.ts
+++ src/utils/get-worker-url.ts
@@ -3,5 +3,5 @@ import { WORKER_BASE_URL } from "@/constants/app";
 export function getWorkerUrl(urlPath: string) {
   let base = WORKER_BASE_URL
   if (!WORKER_BASE_URL) base = process.env.NEXT_PUBLIC_WORKER_URL
-  return `${base}${urlPath || ''}`
+  return `${base}/${urlPath || ''}`
 }
