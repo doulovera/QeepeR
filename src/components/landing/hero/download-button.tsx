0d873b7a6f906012df79c0498f658a1feccdad94
Douglas Lovera <doulovera@gmail.com>
2025-03-08 11:34:57 -0500

    feat: add PNG option in DownloadButton component

--- src/components/landing/hero/download-button.tsx
+++ src/components/landing/hero/download-button.tsx
@@ -1,20 +1,16 @@
-import { Button } from "@/components/shared/button";
-import { downloadSvg } from "@/utils/download-image";
+import { Button } from '@/components/shared/button'
+import { downloadPng, downloadSvg } from '@/utils/download-image'
 
-export function DownloadButton ({ svg }: { svg: string | null }) {
+export function DownloadButton({ svg }: { svg: string | null }) {
   const handleSave = () => {
     if (!svg) return
-    downloadSvg('qr-code', svg)
+    // downloadSvg('qr-code', svg)
+    downloadPng('qr-code', svg, { scale: 5 })
   }
 
   return (
-    <Button
-      disabled={!svg}
-      onClick={handleSave}
-    >
-      <span className="flex justify-center w-full text-center">
-        Download
-      </span>
+    <Button disabled={!svg} onClick={handleSave}>
+      <span className="flex justify-center w-full text-center">Download</span>
     </Button>
   )
 }
--- src/utils/download-image.ts
+++ src/utils/download-image.ts
@@ -1,6 +1,6 @@
 'use client'
 
-export const downloadSvg = (name: string, svg: string) => {
+export function downloadSvg(name: string, svg: string) {
   try {
     const blob = new Blob([svg], { type: 'image/svg+xml' })
     const url = URL.createObjectURL(blob)
@@ -12,6 +12,39 @@ export const downloadSvg = (name: string, svg: string) => {
     return true
   } catch (error) {
     console.error('Error downloading SVG:', error)
+    return false
+  }
+}
+
+type DownloadPngOptions = {
+  scale?: number
+}
+export function downloadPng(
+  name: string,
+  svg: string,
+  { scale }: DownloadPngOptions = {},
+) {
+  try {
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    if (!ctx) return false
+
+    const img = new Image()
+    img.src = `data:image/svg+xml;base64,${btoa(svg)}`
+    img.onload = () => {
+      canvas.width = img.width * (scale || 1)
+      canvas.height = img.height * (scale || 1)
+      ctx.drawImage(img, 0, 0)
+
+      const a = document.createElement('a')
+      a.href = canvas.toDataURL('image/png')
+      a.download = `${name}.png`
+      a.click()
+    }
+
+    return true
+  } catch (error) {
+    console.error('Error downloading PNG:', error)
     return false
   }
 }
