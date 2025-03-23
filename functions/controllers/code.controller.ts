5208b3a3e6c4835e3b612c9cd0b19aa44f42426b
Douglas Lovera <doulovera@gmail.com>
2025-03-23 18:08:13 -0500

    refactor: standardize import statements and improve error handling in QR link functions

--- functions/controllers/code.controller.ts
+++ functions/controllers/code.controller.ts
@@ -1,13 +1,19 @@
-import type { HonoContext, QeeperCtx } from "../types"
+import type { HonoContext, QeeperCtx } from '../types'
 
-import { Hono } from "hono"
+import { Hono } from 'hono'
 
-import { successPayload, errorPayload } from "../lib/response"
-import ApplicationError from "../lib/error"
-import { deleteQrLink, generateQrLink, getQrLink, getQrViews, updateQrLink } from "../lib/qr-link"
+import { successPayload, errorPayload } from '../lib/response'
+import ApplicationError from '../lib/error'
+import {
+  deleteQrLink,
+  generateQrLink,
+  getQrLink,
+  getQrViews,
+  updateQrLink,
+} from '../lib/qr-link'
 
-import { API_RESPONSE } from "../constants/errors"
-import { extractAppUrl } from "../utils/extract-app-url"
+import { API_RESPONSE } from '../constants/errors'
+import { extractAppUrl } from '../utils/extract-app-url'
 
 export default function genRoutes() {
   const code = new Hono<HonoContext>()
@@ -32,12 +38,22 @@ export default function genRoutes() {
 export const createDynamicQR = async (c: QeeperCtx) => {
   try {
     const body = await c.req.json()
-    if (!body) throw new ApplicationError(API_RESPONSE.MISSING_BODY.TITLE, API_RESPONSE.MISSING_BODY.MESSAGE, 400)
+    if (!body)
+      throw new ApplicationError(
+        API_RESPONSE.MISSING_BODY.TITLE,
+        API_RESPONSE.MISSING_BODY.MESSAGE,
+        400,
+      )
 
-    const { url } = body
-    if (!url) throw new ApplicationError(API_RESPONSE.MISSING_BODY_KEY.TITLE, API_RESPONSE.MISSING_BODY_KEY.MESSAGE('url'), 400)
+    const { url, key: overridedKey } = body
+    if (!url)
+      throw new ApplicationError(
+        API_RESPONSE.MISSING_BODY_KEY.TITLE,
+        API_RESPONSE.MISSING_BODY_KEY.MESSAGE('url'),
+        400,
+      )
 
-    const key = await generateQrLink(c, url)
+    const key = await generateQrLink(c, url, overridedKey)
     const appUrl = extractAppUrl(c.req.url)
     const qrUrl = `${appUrl}/${key}`
 
@@ -56,7 +72,12 @@ export const createDynamicQR = async (c: QeeperCtx) => {
 export const getInfoDynamicQR = async (c: QeeperCtx) => {
   try {
     const key = c.req.param('key')
-    if (!key) throw new ApplicationError(API_RESPONSE.MISSING_BODY_KEY.TITLE, API_RESPONSE.MISSING_BODY_KEY.MESSAGE('key'), 400)
+    if (!key)
+      throw new ApplicationError(
+        API_RESPONSE.MISSING_BODY_KEY.TITLE,
+        API_RESPONSE.MISSING_BODY_KEY.MESSAGE('key'),
+        400,
+      )
 
     const destinationUrl = await getQrLink(c, key)
     const views = await getQrViews(c, key)
@@ -65,26 +86,40 @@ export const getInfoDynamicQR = async (c: QeeperCtx) => {
     const response = {
       destinationUrl,
       views,
-      viewsDisabled: isViewsDisabled
+      viewsDisabled: isViewsDisabled,
     }
 
     return successPayload(c, { success: true, response })
   } catch (error) {
     return errorPayload(c, error as Error)
   }
-
 }
 
 export const updateUrlDynamicQR = async (c: QeeperCtx) => {
   try {
     const body = await c.req.json()
-    if (!body) throw new ApplicationError(API_RESPONSE.MISSING_BODY.TITLE, API_RESPONSE.MISSING_BODY.MESSAGE, 400)
+    if (!body)
+      throw new ApplicationError(
+        API_RESPONSE.MISSING_BODY.TITLE,
+        API_RESPONSE.MISSING_BODY.MESSAGE,
+        400,
+      )
 
     const key = c.req.param('key')
-    if (!key) throw new ApplicationError(API_RESPONSE.MISSING_BODY_KEY.TITLE, API_RESPONSE.MISSING_BODY_KEY.MESSAGE('key'), 400)
+    if (!key)
+      throw new ApplicationError(
+        API_RESPONSE.MISSING_BODY_KEY.TITLE,
+        API_RESPONSE.MISSING_BODY_KEY.MESSAGE('key'),
+        400,
+      )
 
     const qr = await getQrLink(c, key)
-    if (!qr) throw new ApplicationError(API_RESPONSE.NOT_FOUND.TITLE, API_RESPONSE.NOT_FOUND.MESSAGE, 404)
+    if (!qr)
+      throw new ApplicationError(
+        API_RESPONSE.NOT_FOUND.TITLE,
+        API_RESPONSE.NOT_FOUND.MESSAGE,
+        404,
+      )
 
     const { url: newUrl } = body
     const response = await updateQrLink(c, key, newUrl)
@@ -98,10 +133,20 @@ export const updateUrlDynamicQR = async (c: QeeperCtx) => {
 export const deleteDynamicQR = async (c: QeeperCtx) => {
   try {
     const key = c.req.param('key')
-    if (!key) throw new ApplicationError(API_RESPONSE.MISSING_BODY_KEY.TITLE, API_RESPONSE.MISSING_BODY_KEY.MESSAGE('key'), 400)
+    if (!key)
+      throw new ApplicationError(
+        API_RESPONSE.MISSING_BODY_KEY.TITLE,
+        API_RESPONSE.MISSING_BODY_KEY.MESSAGE('key'),
+        400,
+      )
 
     const qr = await getQrLink(c, key)
-    if (!qr) throw new ApplicationError(API_RESPONSE.NOT_FOUND.TITLE, API_RESPONSE.NOT_FOUND.MESSAGE, 404)
+    if (!qr)
+      throw new ApplicationError(
+        API_RESPONSE.NOT_FOUND.TITLE,
+        API_RESPONSE.NOT_FOUND.MESSAGE,
+        404,
+      )
 
     const response = await deleteQrLink(c, key)
 
